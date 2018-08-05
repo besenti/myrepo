2901aaeb1e8fe5c679a86735a17eff800781d488
Besenti Samson <bsamson@fnb.co.za>
2018-08-06 00:30:54 +0200

    added new API

--- src/main/java/com/example/helloworld/controller/Greeting.java
+++ src/main/java/com/example/helloworld/controller/Greeting.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "greeting")
 public class Greeting {
 
     @RequestMapping(value = "/say")
     public String say(){
         return "Hello world folks!";
+    }
+
+    @RequestMapping(value = "/error")
+    public String error(){
+        return "Problems!!!";
     }
 }
